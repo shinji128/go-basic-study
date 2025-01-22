0d22adbf9d0b656d289e7ffc5b1d853fe714ee38
miura <shinji65536@gmail.com>
2025-01-23 00:02:53 +0900

    1-2 ハンドラを増やそう

--- myapi/main.go
+++ myapi/main.go
@@ -1,16 +1,18 @@
 package main
+
 import (
-  "io"
   "log"
   "net/http"
+  "github.com/shinji128/go-basic-study/myapi/handlers"
 )
 
 func main() {
-  helloHandler := func(w http.ResponseWriter, req *http.Request) {
-    io.WriteString(w, "Hello, world!\n")
-  }
-
-  http.HandleFunc("/", helloHandler)
+  http.HandleFunc("/hello", handlers.HelloHandler)
+  http.HandleFunc("/article", handlers.PostArticleHandler)
+  http.HandleFunc("/article/list", handlers.ArticleListHandler)
+  http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+  http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+  http.HandleFunc("/comment", handlers.PostCommentHandler)
 
   log.Println("server start at port 8080")
   log.Fatal(http.ListenAndServe(":8080", nil))
