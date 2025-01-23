9b311949c5f41359dd3cd9e82eb0f166c1dcfd1e
miura <shinji65536@gmail.com>
2025-01-23 18:20:57 +0900

    1-5 パスパラメータを取得できるようになろう

--- myapi/handlers/handlers.go
+++ myapi/handlers/handlers.go
@@ -4,6 +4,8 @@ import (
   "fmt"
   "io"
   "net/http"
+  "strconv"
+  "github.com/gorilla/mux"
 )
 // /hello のハンドラ
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
@@ -18,16 +20,23 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
   io.WriteString(w, "Article List\n")
 }
-// /article/1 のハンドラ
+// /article/{:id} のハンドラ
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-  articleID := 1
+  // strconv.Atoiでidをstringからintに変換 intに変換できない場合はerrにエラーが入る
+  articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+  if err != nil {
+    http.Error(w, "Invalid article ID", http.StatusBadRequest)
+    return
+  }
   resString := fmt.Sprintf("Article No.%d\n" , articleID)
   io.WriteString(w, resString)
 }
+
 // /article/nice のハンドラ
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
   io.WriteString(w, "Posting Nice...\n")
 }
+
 // /comment のハンドラ
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
   io.WriteString(w, "Posting Comment...\n")
