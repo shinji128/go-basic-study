74b16a66cf9ea8bd1b0c09598081b5ca4a3d6230
miura <shinji65536@gmail.com>
2025-01-23 16:50:26 +0900

    1-4 gorilla/muxパッケージを使ってみよう

--- myapi/go.mod
+++ myapi/go.mod
@@ -1,3 +1,5 @@
 module github.com/shinji128/go-basic-study/myapi
 
 go 1.23.5
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- myapi/handlers/handlers.go
+++ myapi/handlers/handlers.go
@@ -7,52 +7,28 @@ import (
 )
 // /hello のハンドラ
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-  if req.Method == http.MethodGet {
-    io.WriteString(w, "Hello, world!\n")
-  } else {
-    http.Error(w, "Invalid Allowed", http.StatusMethodNotAllowed)
-  }
+  io.WriteString(w, "Hello, world!\n")
 }
 // /article のハンドラ
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-  if req.Method == http.MethodPost {
-    io.WriteString(w, "Posting, Article...\n")
-  } else {
-    http.Error(w, "Invalid Allowed", http.StatusMethodNotAllowed)
-  }
+  io.WriteString(w, "Posting, Article...\n")
 }
 
 // /article/list のハンドラ
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-  if req.Method == http.MethodGet {
-    io.WriteString(w, "Article List\n")
-  } else {
-    http.Error(w, "Invalid Allowed", http.StatusMethodNotAllowed)
-  }
+  io.WriteString(w, "Article List\n")
 }
 // /article/1 のハンドラ
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-  if req.Method == http.MethodGet {
-    articleID := 1
-    resString := fmt.Sprintf("Article No.%d\n" , articleID)
-    io.WriteString(w, resString)
-  } else {
-    http.Error(w, "Invalid Allowed", http.StatusMethodNotAllowed)
-  }
+  articleID := 1
+  resString := fmt.Sprintf("Article No.%d\n" , articleID)
+  io.WriteString(w, resString)
 }
 // /article/nice のハンドラ
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-  if req.Method == http.MethodPost {
-    io.WriteString(w, "Posting Nice...\n")
-  } else {
-    http.Error(w, "Invalid Allowed", http.StatusMethodNotAllowed)
-  }
+  io.WriteString(w, "Posting Nice...\n")
 }
 // /comment のハンドラ
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-  if req.Method == http.MethodPost {
-    io.WriteString(w, "Posting Comment...\n")
-  } else {
-    http.Error(w, "Invalid Allowed", http.StatusMethodNotAllowed)
-  }
+  io.WriteString(w, "Posting Comment...\n")
 }
